969f292dab0903486e13000379f3656d9e6bce23
Daniel <josetisrey@gmail.com>
2018-11-08 12:23:53 -0600

    menu y insertar

--- TercerProyecto.rb
+++ TercerProyecto.rb
@@ -1,190 +1,193 @@
 require 'rubygems'
-
-
-
 require 'terminal-table'
 
-
-
 def limpiar_pantalla
-
     system('clear')
-
+end
+#INSERTA PILA
+def insertar_pila(cadena,pila)
+  cadena.each do |x|
+    nodo = {
+      valor: x,
+      posicion: pila[:size], 
+      siguiente: nil,
+    }
+    if pila[:vacia]
+      pila[:tope] = nodo
+      pila[:size]+=1
+      pila[:vacia]=false
+    else
+      tope = pila[:tope]
+      nodo[:siguiente] = tope
+      pila[:tope] = nodo
+      pila[:size]+=1
+    end
+  end
+  puts pila
+  gets
+end
+#INSERTAR COLA
+def insertar_cola(cadena,cola)
+  cadena.each do |x|
+    nodo = {
+      valor: x,
+      posicion: cola[:size], 
+      siguiente: nil,
+    }
+    if cola[:vacia]
+      cola[:tope] = nodo
+      cola[:final] = nodo
+      cola[:size]+=1
+      cola[:vacia]=false
+    else
+      final = cola[:final]
+      final[:siguiente] = nodo
+      cola[:final] = nodo
+      cola[:size] +=1 
+    end
+  end
+  puts cola
+  gets
+end
+#INSERTAR LISTA
+def insertar_lista(cadena,lista)
+  cadena.each do |x|
+    nodo = {
+      valor: x,
+      posicion: lista[:size], 
+      siguiente: nil,
+    }
+    if lista[:vacia]
+      lista[:tope] = nodo
+      lista[:final] = nodo
+      lista[:size]+=1
+      lista[:vacia]=false
+    else
+      tope = lista[:tope]
+      nodo[:siguiente] = tope
+      lista[:tope] = nodo
+      lista[:size]+=1
+    end
+  end
+  puts lista
+  gets
 end
 
-def novalido
+#INGRESE DE DATOS
+def ingreso_de_datos(cola,pila,lista)
+  puts 'Ingresar datos'
+  cadena = gets.chomp
+  cadena1 = cadena.split(',')
+  insertar_pila(cadena1,pila)
+  insertar_cola(cadena1,cola)
+  insertar_lista(cadena1,lista)
 
-    puts "LA OPCION INGRESADA NO EXISTE"
-
-    #Limpiar pantalla y una pausa
-
-    gets()
-
-    limpiar_pantalla
-
+  gets
 end
-
-def ingreso_de_datos()
-  puts "\n"
-  puts "+++++INGRESO DE DATOS+++++"
-  puts "\n"
-  puts "Ingrese los Numeros a Ordenar: "
-  n = gets.chomp
-  limpiar_pantalla
-end
-
+#ORDENAR TODOS LOS DATOS
 def ordenar_todos_los_datos()
-  puts "\n"
-  puts "+++++ORDENANR TODOS LOS DATOS+++++"
-  puts "\n"
-  puts "Ingrese los Elementos a Ordenar: "
-  a = gets.chomp
   limpiar_pantalla
-end
+  begin
+  tabla = Terminal::Table.new do |a|
+  a.title= 'Ordenar todos los datos'
+  a.headings = [{value:'No.', alignment: :center},{value:'Estructura', alignment: :center}]
 
+  a.add_row([{value:'1', alignment: :center},'Cola.' ])
+  a.add_row([{value:'2', alignment: :center},'Pila.'])
+  a.add_row([{value:'3', alignment: :center},'Lista.'])
+  a.add_row([{value:'4', alignment: :center},'Salir.' ])
+end 
+  puts tabla
+  puts "Ingrese una estructura: " 
+  opc = gets.chomp
+  limpiar_pantalla()
+  case opc
+  when '1'
+  when '2'
+  when '3'
+  else
+    'La opcion no es valida vuelva a ingresar'
+  end
+end while opc!='4' #fin begin
+end
+#ORDENAR PASO A PASO
 def ordenar_paso_a_paso()
 
-    begin
+limpiar_pantalla
+  begin
+  tabla = Terminal::Table.new do |a|
+  a.title= 'Ordenar paso a paso'
+  a.headings = [{value:'No.', alignment: :center},{value:'Estructura', alignment: :center}]
 
-      tabla = Terminal::Table.new do |t|
-
-
-        t.title = "ORDENAR PASO A PASO "
-        #Menu de ordenar paso a paso
-
-        t.headings = ([
-
-          ['\t¿En Cual Estructura desea Ordenar?'],
-
-         ["1) Pila"],
-
-         ["2) Cola"],
-
-         ["3) Lista"],
-
-         ["4) Salir"],
-
-         ["Seleccione una opcion: "],
-
-        ])
-
-        #uso de case para crear el menu
-
-      end
-
-      puts tabla
-
-      opciones = gets.chomp
-
-        limpiar_pantalla
-
-        case opciones
-
-        when '1'
-            puts "\n"
-            puts"***Estructura Pila***"
-            puts"\n"
-
-            #Limpiar pantalla y una pausa
-
-            gets()
-
-            limpiar_pantalla
-
-        when '2'
-          puts "\n"
-          puts"***Estructura Cola***"
-          puts"\n"
-
-            #Limpiar pantalla y una pausa
-
-            gets()
-
-            limpiar_pantalla
-
-        when '3'
-
-          puts "\n"
-          puts"***Estructura Lista***"
-          puts"\n"
-
-            #Limpiar pantalla y una pausa
-
-            gets()
-
-            limpiar_pantalla
-
-        when '4'
-
-            puts"Progra Terminal"
-
-
-        else novalido
-
-      end
-
-    end while (opciones!='4')
-
+  a.add_row([{value:'1', alignment: :center},'Cola.' ])
+  a.add_row([{value:'2', alignment: :center},'Pila.'])
+  a.add_row([{value:'3', alignment: :center},'Lista.'])
+  a.add_row([{value:'4', alignment: :center},'Salir.' ])
+end 
+  puts tabla
+  puts "Ingrese una estructura: " 
+  opc = gets.chomp
+  limpiar_pantalla()
+  case opc
+  when '1'
+  when '2'
+  when '3'
+  else
+    'La opcion no es valida vuelva a ingresar'
+  end
+end while opc!='4' #fin begin
 end
 
 #Menu Principal
+cola = {
+  tope: nil,
+  final: nil,
+  size: 0,
+  vacia: true
+}
+pila = {
+  tope: nil,
+  size: 0,
+  vacia: true
+}
+lista = {
+  tope: nil,
+  final: nil,
+  size: 0,
+  vacia: true
+}
 
 begin
+  limpiar_pantalla
+  tabla = Terminal::Table.new do |a|
+  a.title= 'Lista De Valores Enteros'
+  a.headings = ['No.',{value:'Opciones', alignment: :center}]
 
-  tabla = Terminal::Table.new do |t|
-
-    t.title = "**********TERCER PROYECTO********** "
-
-    t.headings = ([
-
-
-     [ '1) Ingreso de Datos'],
-
-     ['2) Ordenar todos los Datos'],
-
-     ['3) Ordenar paso a paso'],
-
-     ['4) Salir'],
-
-     ['Seleccione una Opcion: '],
-
-    ])
-
-
+  a.add_row([{value:'1', alignment: :center},'Ingreso de Datos.' ])
+  a.add_row([{value:'2', alignment: :center},'Odernar todos los datos.'])
+  a.add_row([{value:'3', alignment: :center},'Ordenar paso a paso.'])
+  a.add_row([{value:'4', alignment: :center},'Salir.' ])
+end
     #uso de case para crear el menu
 
-  end
-
   puts tabla
-
+  print 'Ingrese una opcion: '
     opciones = gets.chomp
 
     #uso de case para crear el menu
+    limpiar_pantalla()
 
     case opciones
     when '1'
-
-      limpiar_pantalla
-
-      ingreso_de_datos()
-
-    when '2'
-
-      limpiar_pantalla
+      ingreso_de_datos(cola,pila,lista)
+    when '2' 
       ordenar_todos_los_datos()
-
     when '3'
-
-        limpiar_pantalla
-
         ordenar_paso_a_paso()
-
     when '4'
-
         puts "Programa Terminado"
-
-    else novalido
-
+    else 
+      puts 'Esta opcion no es valida vuelva a intentar'
+      gets
     end
-
   end while (opciones!='4')
